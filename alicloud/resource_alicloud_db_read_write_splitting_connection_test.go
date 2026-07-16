7a9e15fff2eadf8c94a04f8a3a087b41a6b05343
chaitiansheng0524 <cts01586841@alibaba-inc.com>
2026-07-16 16:08:56 +0800

    testcase: fix DBReadWriteSplittingConnection - sdkv2 syntax + hardcode mssql.x4.medium.e2

--- alicloud/resource_alicloud_db_read_write_splitting_connection_test.go
+++ alicloud/resource_alicloud_db_read_write_splitting_connection_test.go
@@ -81,14 +81,6 @@ data "alicloud_db_zones" "default"{
 	category                 = "AlwaysOn"
 }
 
-data "alicloud_db_instance_classes" "master" {
-    zone_id                  = data.alicloud_db_zones.default.zones[0].id
-	engine                   = "SQLServer"
-	engine_version           = "2017_ent"
- 	db_instance_storage_type = "cloud_essd"
-	instance_charge_type     = "PostPaid"
-	category                 = "AlwaysOn"
-}
 
 data "alicloud_vswitches" "default" {
   zone_id = data.alicloud_db_zones.default.zones[0].id
@@ -107,7 +99,7 @@ resource "alicloud_db_instance" "default" {
     engine                   = "SQLServer"
 	engine_version           = "2017_ent"
  	db_instance_storage_type = "cloud_essd"
-	instance_type            = "mssql.x4.large.e2"
+instance_type            = "mssql.x4.medium.e2"
 	instance_storage         = 20
 	vswitch_id               = data.alicloud_vswitches.default.vswitches[0].vswitch_id
 	instance_name            = var.name
